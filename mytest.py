77176290d16c4ce26fca8920926084dfa690ea0d
JasonFems <femijasonadewuyi@gmail.com>
2024-09-10 15:42:17 +0100

    My ffirst Test Automation

---
+++ mytest.py
@@ -0,0 +1,11 @@
+import pytest
+import mycal
+
+
+def test_check_it_return_addition():
+    result = mycal.add(2, 3)
+    assert result == 5
+
+def test_check_it_return_subtraction():
+    result = mycal.subtract(10, 4)
+    assert result == 6
