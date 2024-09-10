0638f753c4d26aaa9609b1b42555736120fd61eb
JasonFems <femijasonadewuyi@gmail.com>
2024-09-10 16:28:08 +0100

    Added a new feature

--- mytest.py
+++ mytest.py
@@ -9,3 +9,7 @@ def test_check_it_return_addition():
 def test_check_it_return_subtraction():
     result = mycal.subtract(10, 4)
     assert result == 6
+
+def test_check_it_return():
+    result = mycal.multiply(5, 8)
+    assert result == 40
